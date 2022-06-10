60fa4a201f5b0fb244a5fec3834f76b3253a8e2b
ElfSolidarity <akshay.seetharam628@gmail.com>
2022-06-09 22:06:58 -0700

    added 'simple-fitness-tracking.py' for only tracking fitnesses TODO: track which mutations are taking over TO ASK: randomness, poisson random sample clarification, clarification of exponential growth

--- simple-fitness-tracking.py
+++ simple-fitness-tracking.py
@@ -1,6 +1,7 @@
 import random
 from math import exp
 import time
+from matplotlib import pyplot as plt
 
 def next_gen(population, fitnesses, Ub, b, Ud, d):
     reproduced = {}
@@ -10,7 +11,7 @@ def next_gen(population, fitnesses, Ub, b, Ud, d):
     # mutate some of the bacteria positively and some negatively
     sampled = {}
     for fitness in reproduced:
-        sampled[fitness + b] = Ub * reproduced[fitness]
+        sampled[fitness + b] = Ub * reproduced[fitness] # TODO: one-by-one sampling is exactly the same as binomial
         sampled[fitness + d] = Ud * reproduced[fitness]
         sampled[fitness] = reproduced[fitness] - (sampled[fitness + b] + sampled[fitness + d])
 
@@ -24,22 +25,29 @@ def next_gen(population, fitnesses, Ub, b, Ud, d):
 
 if __name__=='__main__':
     # This uses the format of only keeping track of fitness quantities, disregarding the organism and genome level
-    population = 10 ** 9
-    generations = 10
-    starting_fitness = 0.5
+    population = 10
+    generations = 100
+    starting_fitness = 0.01
     Ub = 0.01 # probability of beneficial mutation
     b = 0.01 # affect of beneficial mutation to fitness
     Ud = 0.1 # probability of detrimental mutation
     d = -1 * 0.02 # affect of detrimental mutation to fitness
 
 
-    fitnesses = [{starting_fitness: population}]
+    fitnesses = [{starting_fitness: population}] # key: value, fitness: # of individuals with fitness
 
     start_time = time.time()
 
     for i in range(generations):
         next_gen(population, fitnesses, Ub, b, Ud, d)
+        print(f'gen {i} done')
 
-    print(fitnesses)
+    mean_fitnesses = []
+    for generation in fitnesses:
+        # print(generation)
+        mean_fitnesses.append(sum([fitness * proportion for fitness, proportion in generation.items()]))
 
     print("--- %s seconds ---" % (time.time() - start_time))
+
+    plt.scatter(range(len(fitnesses)), mean_fitnesses)
+    plt.show()
