2c92791a2c8e2e0baadd17678c82d25be1497d59
ElfSolidarity <akshay.seetharam628@gmail.com>
2022-07-21 22:52:19 -0700

    reproduce population

--- full-lineage-model.py
+++ full-lineage-model.py
@@ -30,7 +30,31 @@ class Population:
         self.generations = [{Lineage(set(), fitness, Ub, b_mean, b_stdev): size}]
 
     def reproduce(self) -> None:
-        pass
+        self.generations.append({})
+
+        # reproduce each lineage in the previous generation
+        for lineage, count in self.generations[-2].items():
+            population_growth = np.exp(lineage.fitness) * count
+            self.generations[-1][lineage] = population_growth
+
+        # mutate a proportion of each lineage
+        for lineage, count in self.generations[-1].items():
+            num_mutated = int(np.random.binomial(count, Ub))
+            for i in range(num_mutated):
+                new_lineage = lineage.mutate()
+                self.generations[-1][new_lineage] = 1
+                self.generations[-1][lineage] -= 1
+
+        # remove the lineages with 0 population to prevent any chicanery
+        for lineage, count, in self.generations[-1].items():
+            if count == 0:
+                del self.generations[-1][lineage]
+
+        # prune population down to size
+        current_population = sum(self.generations[-1].values())
+        correction_factor = self.size / current_population
+        for lineage in self.generations[-1]:
+            self.generations[-1][lineage] *= correction_factor
 
 class Simulation:
     def __init__(self, runs: int, num_gens: int, size: int, *args):
