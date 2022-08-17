959eabca5d7d1b4a6334c1dd6862692cf784983f
ElfSolidarity <akshay.seetharam628@gmail.com>
2022-08-17 15:01:18 -0700

    plot fitnesses and save image to fitnesses-fgm.png

--- fisher-geometric-model.py
+++ fisher-geometric-model.py
@@ -1,6 +1,8 @@
 import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
 
-def next_gen(population, n, r):
+def next_gen():
     """
     Calculate the next generation of the population
     """
@@ -9,10 +11,30 @@ def next_gen(population, n, r):
         vector *= r / np.linalg.norm(vector) # scale to r
         i = [i[j] + vector[j] for j in range(n)] # mutate the individual
 
+def plot_fitnesses():
+    """
+    Plot the fitnesses of the population
+    """
+    # Set the figure size
+    plt.rcParams["figure.figsize"] = [7.50, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+
+    logarithmic = np.log(fitnesses)
+
+    # Pandas dataframe
+    data = pd.DataFrame({f'Gen {i}': logarithmic[i] for i in range(len(logarithmic))})
+
+    # Plot the dataframe
+    ax = data[[f'Gen {i}' for i in range(len(logarithmic))]].plot(kind='box', title='boxplot')
+
+    # Display the plot
+    plt.savefig('imgs/fitnesses-fgm.png')
+
 
 if __name__=='__main__':
     """ PARAMETERS """
     # taken from https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4699269/table/T but with population cloud rather than single point
+    global n, a, Q, s0, r, sigma, e, m, N, mu, generations
     n = 2 # dimension of phenotypic space
     a = 1.0 # robustness parameter in fitness function
     Q = 1.0 # epistasis parameter in fitness function
@@ -29,12 +51,14 @@ if __name__=='__main__':
     def fitness(d): # fitness function from paper (input is distance from fitness peak)
         return np.exp(-1 * a * (d ** Q))
 
-    generations = 100 # number of generations to run simulation for
+    generations = 20 # number of generations to run simulation for
     """ PARAMETERS """
 
     """ SIMULATION """
     # initialize population
+    global population
     population = np.random.normal(1.5, 0.5, size=(N, n)) # random population
+    global fitnesses
     fitnesses = np.zeros((generations, N))
     j = 0
     while j < fitnesses.shape[1]:
@@ -42,9 +66,11 @@ if __name__=='__main__':
         j += 1
 
     for i in range(generations):
-        next_gen(population, n, r)
+        next_gen()
         j = 0
         while j < fitnesses.shape[1]:
             fitnesses[i][j] = fitness(np.linalg.norm(population[j])) # calculate fitness of each individual in next generation
             j += 1
+
+    plot_fitnesses()
 
---
+++ test.py
@@ -0,0 +1,8 @@
+def go(n):
+	n += 1
+	print(n)
+
+if __name__=='__main__':
+	n = 2
+	go(n)
+	print(n)
